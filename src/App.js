d736087cd4ca24503018091f629b642e483a28ea
Herless Alvarado <herlessalvarado@gmail.com>
2023-09-27 20:31:45 -0500

    authenticated routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Cookies from "js-cookie";
 
 function App() {
+  const isAuthenticated = !!Cookies.get("jwtToken");
+
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/" element={isAuthenticated ? <Dashboard /> : <Login />} />
     </Routes>
   );
 }
